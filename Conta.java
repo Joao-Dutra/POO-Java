36aa04e0b815d7e597833bbe99e9c344c1ef1ec5
Joao-Dutra <54649142+Joao-Dutra@users.noreply.github.com>
2023-09-15 20:44:27 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -1,38 +1,67 @@
 package poo;
 
 public class Conta {
-    
+
     // atributos
-    
-    int numero;
-    Cliente titular;
-    double saldo;
-    
+    private int numero;
+    private Cliente titular;
+    private double saldo;
+
     // metodos
+    public void setSaldo(double s) {
+        if (s >= 0) {
+            this.saldo = s;
+        } else {
+            this.saldo = 0;
+            System.out.println("Impossível inserir um valor negativo");
+        }
+    }
+
+    public double getSaldo(){
+        return this.saldo;
+    }
     
-    void consultarSaldo(){
+    public void setNumero(int n) {
+        this.numero = n;
+    }
+    
+    public int getNumero(){
+        return this.numero;
+    }
+    
+    public void setTitular(Cliente t) {
+        this.titular = t;
+    }
+    
+    public Cliente getTitular(){
+        return this.titular;
+    }
+    
+    public void consultarSaldo() {
         System.out.println("O saldo é " + this.saldo);
     }
-    boolean sacarValor(double valorSaque){            
+
+    boolean sacarValor(double valorSaque) {
         if (valorSaque > this.saldo) {
             System.out.println("Não tem como sacar esse saldo.");
             return false;
-        }
-        else{
+        } else {
             this.saldo -= valorSaque;
             System.out.println("Saldo em sua conta : " + this.saldo);
             return true;
         }
     }
-    void depositarValor(double valorDepositar){
+
+    void depositarValor(double valorDepositar) {
         this.saldo += valorDepositar;
         System.out.println("Saldo em sua conta : " + this.saldo);
     }
-    void transferirValor(double valorTransferir, Conta destino){
-       boolean retirou = this.sacarValor(valorTransferir);
+
+    void transferirValor(double valorTransferir, Conta destino) {
+        boolean retirou = this.sacarValor(valorTransferir);
         if (retirou == false) {
             System.out.println("Saldo Insuficiente. Impossível Transferir!");
-        }else{
+        } else {
             destino.depositarValor(valorTransferir);
         }
     }
