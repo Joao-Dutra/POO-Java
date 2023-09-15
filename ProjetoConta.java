37371b5b8bac50b7dc1966949f96233dad3557cc
Joao-Dutra <54649142+Joao-Dutra@users.noreply.github.com>
2023-09-15 20:44:37 -0300

    Update ProjetoConta.java

--- ProjetoConta.java
+++ ProjetoConta.java
@@ -2,51 +2,46 @@ package poo;
 
 import java.util.Scanner;
 
-public class  ProjetoConta {
+public class ProjetoConta {
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        
-        
+
         Conta c1 = new Conta();
         Cliente cliente1 = new Cliente();
-        
-        c1.titular = cliente1;
-        c1.numero = 1234;
-        c1.saldo = 1000;
-        
-        
-        
-        c1.titular.nome = "Maria";
-        System.out.println(c1.titular.nome);
-        cliente1.cpf = "555.222.111-20";
-        System.out.println(cliente1.cpf);
-        
-        
-        
+
+        c1.setTitular(cliente1);
+        c1.setNumero(1234);
+        c1.setSaldo(1000);
+
+        System.out.println(c1.getSaldo());
+
+        cliente1.setNome("Maria");
+        System.out.println(cliente1.getNome());
+        cliente1.setCpf("555.222.111-20");
+        System.out.println(cliente1.getCpf());
+
         Conta c2 = new Conta();
         Cliente cliente2 = new Cliente();
-        
-        c2.titular = cliente2;
-        c2.numero = 4321;
-        c2.saldo = 200;
-        
-        System.out.println("A conta " + c1.numero + " é da(o) " + c1.titular + " e ela(o) tem " + c1.saldo + " de saldo em sua conta. ");
-        
-        
+
+        c2.setTitular(cliente2);
+        c2.setNumero(4321);
+        c2.setSaldo(200);
+
+        System.out.println("A conta " + c1.getNumero() + " é da(o) " + c1.getTitular().getNome() + " e ela(o) tem " + c1.getSaldo() + " de saldo em sua conta. ");
+
         c1.consultarSaldo();
-        
-        
+
         System.out.println("Qual o valor a ser sacado?");
         double s = teclado.nextDouble();
         c1.sacarValor(s);
-            
-        
+
         System.out.println("Qual o valor a ser depositado?");
         double d = teclado.nextDouble();
         c1.depositarValor(d);
-        
+
         System.out.println("Qual o valor a ser transferido?");
         double t = teclado.nextDouble();
-        c1.transferirValor(t,c2);
+        c1.transferirValor(t, c2);
     }
 }
