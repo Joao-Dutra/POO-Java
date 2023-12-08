575f728dbaa3161b106a03ee07f552328de1c091
Joao-Dutra <54649142+Joao-Dutra@users.noreply.github.com>
2023-12-08 20:34:48 -0300

    Add files via upload

---
+++ Tratamento De Excecao/Main_Conta (Throw).java
@@ -0,0 +1,9 @@
+package poo;
+
+public class Main_Conta {
+    public static void main(String[] args) {
+        
+        Conta c = new Conta(1234,"Joao",1000.0);
+        c.depositar(-100.0);
+    }
+}
