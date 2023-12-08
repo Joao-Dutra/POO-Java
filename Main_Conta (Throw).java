c6d0716a1e22312367174a6f73f40ba99c389835
Joao-Dutra <54649142+Joao-Dutra@users.noreply.github.com>
2023-12-08 20:34:32 -0300

    Delete Main_Conta (Throw).java

--- Main_Conta (Throw).java
+++
@@ -1,9 +0,0 @@
-package poo;
-
-public class Main_Conta {
-    public static void main(String[] args) {
-        
-        Conta c = new Conta(1234,"Joao",1000.0);
-        c.depositar(-100.0);
-    }
-}
